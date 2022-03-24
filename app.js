9acce7dd4a4469f3366077eaa1a319dc95e661a1
mellito <dayanalexismanrique@hotmail.com>
2022-03-23 20:16:17 -0500

    finish

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ morgan.token('body', (req, res) => {
      return JSON.stringify(req.body)
  }
 });
-app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body '));
+app.use(morgan(`:method :url :status :response-time ms - :res[content-length]  :body `));
 
 
 const person=[
